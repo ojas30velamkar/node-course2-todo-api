a21680e2999d44810c24fc0a29deb0a7e718e977
Ojas Velamkar <ojasvelamkar@Ojass-MBP.gateway>
2017-03-05 21:39:09 +1100

    mlab/heroku config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 //mongoose configuration starts
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://todo-app-user:todo-app-user@ds119020.mlab.com:19020/todo-app-api');
 
 module.exports = {mongoose};
